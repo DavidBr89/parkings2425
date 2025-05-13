01636ab8e4614b232b65e1e2da6b46259703ed61
David Breckx <DavidBr89@users.noreply.github.com>
2025-05-13 12:42:24 +0200

    Changed tsconfig - types

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {initializeApp} from "firebase/app";
-import {initializeAuth, getReactNativePersistence, getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  getAuth,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBX8KFWJbxg0VB6e1mx4TBMmNA9l8f7hj8",
@@ -9,16 +13,14 @@ const firebaseConfig = {
   projectId: "hogent-parkings-a0186",
   storageBucket: "hogent-parkings-a0186.firebasestorage.app",
   messagingSenderId: "6320476371",
-  appId: "1:6320476371:web:bac9ca5e880152b6e50426"
+  appId: "1:6320476371:web:bac9ca5e880152b6e50426",
 };
 
 export const app = initializeApp(firebaseConfig);
 
 initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-})
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
